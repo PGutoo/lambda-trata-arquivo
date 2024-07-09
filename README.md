5c9750b065b1350db83026060785f4ac2bfc35bf
Pedro <pedro.link25@gmail.com>
2024-07-09 18:53:49 -0300

    FEAT - Criando excel

--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 
 ## O que é?
 
-Esta é uma aplicação de processamento de regras de negócio que lida com a validação de CNPJs e a integração com uma API externa.
+Esta é uma aplicação em python que simula uma Lambda AWS. Uma Lambda AWS é um programa orientado a eventos, em computação sem servidor fornecido pela Amazon como uma parte da Amazon Web Services.
+Nessa aplicação disponibilizei um endpoint que servirá como o ponto de entrada da lambda, simulando o ponto de entrada como evento de uma lambda real.
+Por sua vez a aplicação lida com a validação de CNPJs.
 
 ## Pré-requisitos
 
 Python 3.x instalado
 
-## Clone o repositório:
+## Clone o repositório
 
 git clone https://github.com/PGutoo/lambda-trata-arquivo.git
 
-## Instale as bibliotecas necessárias:
+## Instale as bibliotecas necessárias
 
 pip install -r requirements.txt
 
@@ -20,7 +22,6 @@ pip install -r requirements.txt
 
 Abra um terminal na sua IDE e execute o comando ``py .\lambda_function.py``, isso iniciará a aplicação localmente na porta 5000.
 Como a aplicação simula uma lambda AWS, utilize a rota /upload-arquivo como o gatilho inicial do evento de uma lambda, um arquivo xlsx deve ser enviado no body request.
-
 
 ## Contato
 
--- businessrule/process_file.py
+++ businessrule/process_file.py
@@ -12,8 +12,9 @@ class ProcessFile:
     def process_file(self, file):
         coluna_cnpj = pd.read_excel(file)
         cnpjs = coluna_cnpj['CNPJ'].tolist()
+        print(cnpjs)
         valida_cnpj = CNPJ()
-        cria_excel = Workbook()
+        excel = Workbook()
         for cnpj in cnpjs:
 
             print(cnpj)
@@ -21,9 +22,20 @@ class ProcessFile:
             valida_cnpj.validate(cnpj_string)
             dados_cnpj = call_cnpj_api(cnpj_string.zfill(14))
             nome = dados_cnpj.get('nome')
+            logradouro = dados_cnpj.get('logradouro')
+            bairro = dados_cnpj.get('bairro')
+            uf = dados_cnpj.get('uf')
+            email = dados_cnpj.get('email')
+
             # TODO
             # Retornar Nome, endereço, status, cnpj e email do CNPJ em um excel
             print(nome)
-            planilha = cria_excel.active
-
+            planilha = excel.active
+            planilha['A1'] = 'Nome'
+            planilha['B1'] = 'Logradouro'
+            planilha['C1'] = 'Bairro'
+            planilha['D1'] = 'UF'
+            planilha['E1'] = 'Email'
+            planilha.append([nome, logradouro, bairro, uf, email])
+            excel.save('novo_arquivo.xlsx') # TODO: nome do arquivo unico, onde retornar?
         return True
